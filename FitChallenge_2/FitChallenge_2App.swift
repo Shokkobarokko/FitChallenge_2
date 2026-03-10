223d1691559e973b726891cca9f3515c305fd422
Avetis <avetis.01@mail.ru>
2026-03-10 19:56:04 +0300

    Initial Commit

---
+++ FitChallenge_2/FitChallenge_2App.swift
@@ -0,0 +1,17 @@
+//
+//  FitChallenge_2App.swift
+//  FitChallenge_2
+//
+//  Created by Аветис Парсаданян on 3/10/26.
+//
+
+import SwiftUI
+
+@main
+struct FitChallenge_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
